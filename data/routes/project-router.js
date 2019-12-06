5c356c8e2ebaa7320771a339ace82b1580be0902
coryortega <coryortega3@gmail.com>
2019-12-06 09:28:56 -0800

    met mvp

--- data/routes/project-router.js
+++ data/routes/project-router.js
@@ -7,23 +7,23 @@ router.use(express.json());
 
 router.post('/', (req, res) => {
     
-    const postData = req.body;
+    const projectData = req.body;
 
-    if (!postData.name || !postData.description) {
+    if (!projectData.name || !projectData.description) {
         res
         .status(400)
-        .json({errorMessage: "Please provide title and contents for the posts"});
+        .json({errorMessage: "Please provide name and description for the project"});
     } else {
-        Projects.insert(postData)
-        .then(post =>{
-            res.status(201).json(post)
+        Projects.insert(projectData)
+        .then(project =>{
+            res.status(201).json(project)
         })
         .catch(
             error => {
                 console.log('error', error);
                 res
                 .status(500)
-                .json({ errorMessage: "There was an error while saving the post to the database"});
+                .json({ errorMessage: "There was an error while saving the project to the database"});
             }); 
     
     }
@@ -37,12 +37,65 @@ router.get('/', (req, res) => {
         res.status(200).json(projects);
         })
         .catch(error => {
-        // log error to database
         console.log(error);
         res.status(500).json({
-            message: 'Error retrieving the posts',
+            message: 'Error retrieving the projects',
         });
         });
     });
+
+    router.get('/:id', (req, res) => {
+        Projects.get(req.params.id)
+        .then(project => {
+          if (project) {
+            res.status(200).json(project);
+          } else {
+            res.status(404).json({ message: 'project not found' });
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({
+            message: 'Error retrieving the project',
+          });
+        });
+      });
+
+      router.delete('/:id', (req, res) => {
+        Projects.remove(req.params.id)
+         .then(count => {
+           if (count > 0) {
+             res.status(200).json({ message: 'project deleted' });
+           } else {
+             res.status(404).json({ message: 'The project could not be found' });
+           }
+         })
+         .catch(error => {
+           console.log(error);
+           res.status(500).json({
+             message: 'Error removing the project',
+           });
+         });
+       });
+
+       router.put('/:id', (req, res) => {
+        const changes = req.body;
+       Projects.update(req.params.id, changes)
+        .then(project => {
+          if (!project) {
+            res.status(404).json({ message: 'The project could not be found' });
+          } else if(!changes.name || !changes.description){
+            res.status(400).json({ message: "update name or description"})
+          } else {
+            res.status(200).json(project);
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({
+            message: 'Error updating the project',
+          });
+        });
+      });
 
 module.exports = router;
